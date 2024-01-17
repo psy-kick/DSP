14c94c416a8a6650c94b8caf8da9b32c45b9503f
Suhas Aditya SM <suhasadityasm@gmail.com>
2024-01-17 00:42:31 +0000

    Done

--- Kepler/Assets/Scripts/Scanner.cs
+++ Kepler/Assets/Scripts/Scanner.cs
@@ -19,11 +19,6 @@ public class Scanner : MonoBehaviour
     GameObject Canvas;
     [SerializeField]
     TextMeshProUGUI textMeshProUGUI;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     // Update is called once per frame
     void Update()
     {
